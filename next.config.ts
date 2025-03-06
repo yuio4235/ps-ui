84c939183dbe32d25fccb959c18ecae2198e5bd7
Danny Zhang <yuio4235@gmail.com>
2025-03-06 14:55:08 +0800

    fix pipline

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-	output: "standalone"
+	reactStrictMode: true,
+  distDir: 'build',
+  output: 'standalone',
 };
-module.exports = nextConfig
+
+export default nextConfig;
